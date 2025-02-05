a3c86a6d40d0926b5a2c2c1277841ba8281b904a
matteodragotto <matteodrag8@gmail.com>
2025-02-05 13:51:25 +0100

    [feat] milestone 2

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,7 +1,13 @@
 const connection = require('../data/db')
 
 const index = (req, res) => {
-  res.send('Lista dei post')
+
+  const sql = 'SELECT * FROM posts'
+
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: 'Richiesta al database fallita' })
+    res.json(results)
+  })
 }
 
 const show = (req, res) => {
